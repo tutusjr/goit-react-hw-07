7a6531918ec7f2616dc1d157d73b622681f26f90
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-12-16 16:12:29 +0300

    all done

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,10 +1,18 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../redux/contactsOps"; // fetchContacts fonksiyonunu içeri aktarın
 import { selectFilteredContacts } from "../../redux/contactsSlice";
 import styles from "./ContactsList.module.css";
 import ContactItem from "../ContactItem/ContactItem";
 
 export default function ContactsList() {
+  const dispatch = useDispatch();
   const filteredContacts = useSelector(selectFilteredContacts);
+
+  // Bileşen yüklendiğinde fetchContacts dispatch edilir
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <ul className={styles.ContactsListContainer}>
--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -5,40 +5,36 @@ const API_URL = `https://${import.meta.env.VITE_API_TOKEN}.mockapi.io/contacts`;
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
-  async (_, { rejectWithValue }) => {
+  async (_, thunkAPI) => {
     try {
       const response = await axios.get(API_URL);
       return response.data;
     } catch (e) {
-      const errorMessage = e.response?.data?.message || "Something Went Wrong";
-      return rejectWithValue(errorMessage);
+      return thunkAPI.rejectWithValue(e.message)
     }
   }
 );
 
 export const addContact = createAsyncThunk(
   "contacts/addContact",
-  async (newContact, { rejectWithValue }) => {
+  async (newContact, thunkAPI) => {
     try {
       const response = await axios.post(API_URL, newContact);
       return response.data;
     } catch (e) {
-      const errorMessage = e.response?.data?.message || "Failed to add contact";
-      return rejectWithValue(errorMessage);
+      return thunkAPI.rejectWithValue(e.message)
     }
   }
 );
 
 export const deleteContact = createAsyncThunk(
   "contacts/deleteContacts",
-  async (id, { rejectWithValue }) => {
+  async (id, thunkAPI) => {
     try {
       await axios.delete(`${API_URL}/${id}`);
       return id;
     } catch (e) {
-      const errorMessage =
-        e.response?.data?.message || "Failed to delete contact";
-      return rejectWithValue(errorMessage);
+      return thunkAPI.rejectWithValue(e.message)
     }
   }
 );
