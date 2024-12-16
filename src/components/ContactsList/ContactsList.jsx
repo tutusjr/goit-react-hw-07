b8ae8cd31489de16614475619c0f8e98c156cfd5
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-12-16 16:12:50 +0300

    all done

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchContacts } from "../../redux/contactsOps"; // fetchContacts fonksiyonunu içeri aktarın
+import { fetchContacts } from "../../redux/contactsOps"; 
 import { selectFilteredContacts } from "../../redux/contactsSlice";
 import styles from "./ContactsList.module.css";
 import ContactItem from "../ContactItem/ContactItem";
@@ -9,7 +9,7 @@ export default function ContactsList() {
   const dispatch = useDispatch();
   const filteredContacts = useSelector(selectFilteredContacts);
 
-  // Bileşen yüklendiğinde fetchContacts dispatch edilir
+ 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
